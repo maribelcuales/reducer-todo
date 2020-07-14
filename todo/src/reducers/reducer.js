00fde26290bbb066cc9a157f224dd780ffbef35e
maribelcuales <maribelcuales@gmail.com>
2020-07-15 00:19:00 +0800

    Build initial state object with an array of todo objects.

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,12 +1,20 @@
 import React from "react"; 
 
-const initialState = {
-  item: 'Learn about reducers',
-  completed: false,
-  id: 3892987589
-}
+const initialState = [
+  {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  },
+  {
+    item: 'Learn Redux',
+    completed: false,
+    id: 3892987589
+  } 
+] 
 
 const reducer = (state, action) => {
   return state; 
-}
+}; 
 
+export { initialState, reducer}; 
