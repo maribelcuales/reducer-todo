cd26520f8cd0c7f1c075f718ebbfbd973b267a0b
maribelcuales <maribelcuales@gmail.com>
2020-07-15 00:00:20 +0800

    Deleted unused template code from React.

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,5 +1,11 @@
 import React from "react"; 
 
+const initialState = {
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+}
+
 const reducer = (state, action) => {
   return state; 
 }
