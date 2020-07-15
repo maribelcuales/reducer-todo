103f40ab8e4a0577531e6701746e76a70185b956
maribelcuales <maribelcuales@gmail.com>
2020-07-15 19:00:41 +0800

    Created initialState as an array of todo objects and reducer to handle Add Todo action.

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,19 +1,34 @@
 import React from "react"; 
 
-const initialState = [
-  {
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-  },
-  {
-    item: 'Learn Redux',
-    completed: false,
-    id: 3892987589
-  } 
-] 
+
+const initialState = {
+  todos: [
+    {
+      item: 'Learn about reducers',
+      completed: false,
+      id: 3892987589
+    },
+    {
+      item: 'Learn Redux',
+      completed: false,
+      id: 3892987589
+    }
+  ]
+}
 
 const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      const newTodo = {
+        item: "",
+        completed: false,
+        id: Date.now()
+      }
+      return {
+        ...state,
+        todos: [...state.todo, newTodo]
+      };
+  }
   return state; 
 }; 
 
