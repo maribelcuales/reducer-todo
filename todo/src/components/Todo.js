bcc6cf6f2e9797b623e8f33133ac359efb65b06f
maribelcuales <maribelcuales@gmail.com>
2020-07-16 20:19:15 +0800

    Refactored Todo component and passed in props todoArray from TodoForm.

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,18 +1,11 @@
-import React, { useState, useReducer } from "react";
+import React from "react"; 
 
-import { reducer, initialState } from "../reducers/reducer";
-import TodoForm from './TodoForm'; 
-
-const Todo = () => {
-  const [state, dispatch] = useReducer(reducer, initialState); 
-  console.log(state); 
-
+const Todo = props => {
   return (
     <div>
-      {state.todos.map(todo => (
+      {props.todoArray.map(todo => (
         <h2>Todo Item: {todo.item}</h2>
       ))}
-      <TodoForm /> 
     </div>
   )
 }
