e6b10ffcef84fdbb6222473441f839a402736d1b
maribelcuales <maribelcuales@gmail.com>
2020-07-16 20:23:53 +0800

    Created TodoForm component with form input passing in state managed by reducers and a Todo component.

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,9 +1,28 @@
-import React from "react"; 
+import React, { useState, useReducer } from "react"; 
+import { reducer, initialState } from "../reducers/reducer"; 
+import Todo from './Todo';
 
 function TodoForm() {
+  const [state, dispatch] = useReducer(reducer, initialState); 
+  console.log(state); 
+
+  const [newTodo, setNewTodo] = useState(""); 
 
   return (
-    <h3>Add Todo Form</h3>
+    <div>
+      <h3>Add Todo Form</h3>
+      <form>
+        <input
+          className="todo-input"
+          type="text"
+          name="todoInput"
+          value={newTodo}
+          // onChange={handleChanges}
+        /> 
+        <button>Submit</button>  
+        <Todo todoArray={state.todos}/>
+      </form>
+    </div>
   )
 };
 
