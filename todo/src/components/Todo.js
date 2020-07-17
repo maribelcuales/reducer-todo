01580da1be6580df02cef4a8c3db77d6f1156594
maribelcuales <maribelcuales@gmail.com>
2020-07-17 20:13:24 +0800

    Added a return statement.

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -5,15 +5,15 @@ const Todo = props => {
   console.log(props.toggleItem); 
   return (
     <div className="todo-list">
-      {props.todoArray.map(todo => (
+      {props.todoArray.map(todo => {
+        return (
         <div 
           key={todo.id}
           onClick={() => props.toggleItem(todo.id)} 
-          className={`todo${todo.completed ? "completed" : ""}`
-        }> 
-          <h2>{todo.item}</h2>
+          className={`todo${todo.completed ? "completed" : ""}`}> 
+          <p>{todo.item}</p>
         </div>
-      ))}
+        )})}
     </div>
   )
 }
